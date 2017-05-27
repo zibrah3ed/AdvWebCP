ee95ee428ec508adf58f855e441b03a54c56fe12
zibrah3ed <tyson.funk@smail.rasmussen.edu>
2017-05-27 14:40:09 -0500

    add html to displaylist

--- displaylist.php
+++ displaylist.php
@@ -1,3 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Grocery Getter</title>
+<!-- Jquery header data -->
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+<link href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" rel="stylesheet" type="text/css"/>
+<link href="styles/custom.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<script src="http://code.jquery.com/jquery-1.8.3.min.js" type="text/javascript"></script>
+<script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js" type="text/javascript"></script>
+<script src="javascript/storage.js"></script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+</body>
+
+
+
 <?php
 // Connect to MySQL
 include 'config.php';
@@ -27,3 +52,10 @@ if (mysqli_num_rows($result) > 0) {
 mysqli_close($conn);
 
 ?>
+</div>
+<div data-role="footer">
+  <h4>Tyson Funk 2017</h4>
+</div>
+
+</body>
+<html>
