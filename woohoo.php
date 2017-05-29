38f58c6554f1fb103911a61a294f77ebe67c87ae
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-05-29 18:59:26 -0500

    Use Hard coded values to check functionality

--- woohoo.php
+++ woohoo.php
@@ -7,8 +7,9 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-
-
+// Use Hard coded values to check functionality
+mysqli_query($conn,"INSERT INTO recipes( userID,RecipeName,gfree,feeds,difficulty,directions)
+  VALUES (1234567890,'Help',1,1,1,"hello world")");
 
 mysqli_close($conn);
 ?>
