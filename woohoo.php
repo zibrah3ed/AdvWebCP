650acaf55d0bda568fa4f66e4ab0e6576826e99d
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-05-29 18:50:12 -0500

    change woohoo

--- woohoo.php
+++ woohoo.php
@@ -1,1 +1,14 @@
-<?php phpinfo(); ?>  
+<?php
+include 'config.php';
+include 'opendb.php';
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+
+
+mysqli_close($conn);
+?>
