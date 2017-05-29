dd86db6507a64c10bb405e062ea4638765c822f0
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-05-29 13:47:08 -0500

    Changed connection for add recipe

--- addrecipe.php
+++ addrecipe.php
@@ -1,6 +1,6 @@
 <html>
 		<head>
-<title>Recipe Submission</title>
+<title>Recipe Add</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="styles/custom.css" />
 <link rel="stylesheet" href="themes/rasmussenthemeroller.min.css" />
@@ -18,59 +18,68 @@
 	}
 </style>
 </head>
-<body>
-<div data-role="page" id="recipeSubmit" data-theme="a" data-add-back-btn="true">
-    <div data-role="header" data-add-back-btn="true">
-      <h1>Grocery Getter - Recipes</h1>
-    </div>
-    <div class="logo">
-     	<center>
-              	<img src="themes/images/banner.png" class="img-rounded img-responsive" width="264" height="85" alt="">
-  	</center>
-    </div>
-<?php
-ini_set('display_errors',1);
 
-      // Connect to MySQL
-      include 'config.php';
-      include 'opendb.php';
+	<body>
+		<div id="page" data-role="page" data-theme="a" >
+			<div data-role="header" data-theme="a">
+    		<h1>
+	     		Recipe Add
+				</h1>
+  		</div>
+  	<div class="logo">
+    	<center>
+        <img src="themes/images/banner.png" class="img-rounded img-responsive" width="264" height="85" alt="">
+      </center>
+   	</div>
+		<div data-role="content" data-theme="a" >
+				<?php
+				// Create connection
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+				include 'config.php';
+				include 'opendb.php';
 
-    //User ID is hardcoded for now to show functionality
-    $userID = '1633040277499'
-    $RecipeName = (isset($_POST['recipeTitle'])    ? $_POST['recipeTitle']   : '');
-    $gfree = (isset($_POST['gfree'])    ? $_POST['gfree']   : '');
-    $feeds = (isset($_POST['feeds'])    ? $_POST['feeds']   : '');
-    $difficulty = (isset($_POST['difficulty'])    ? $_POST['difficulty']   : '');
-    $directions = (isset($_POST['directions'])    ? $_POST['directions']   : '');
+				// Check connection
+				if (!$conn) {
+						die("Connection failed: " . mysqli_connect_error());
+				}
+				// User ID hardcoded atm, login page will need to be added
 
-// Insert our data
-$sql = "INSERT INTO recipes( userID,RecipeName,gfree,feeds,difficulty,directions)
-  VALUES ('$userID','$RecipeName','$gfree','$feeds','$difficulty','$directions')";
-  //VALUES (
-  //'{$mysqli->real_escape_string(isset($_POST['userID'])    ? $_POST['userID']   : '')}' ,
-  //'{$mysqli->real_escape_string(isset($_POST['RecipeName'])    ? $_POST['RecipeName']   : '')}'	,
-  //'{$mysqli->real_escape_string(isset($_POST['gfree'])    ? $_POST['gfree']   : '')}' 	,
-  //'{$mysqli->real_escape_string(isset($_POST['feeds'])    ? $_POST['feeds']   : '')}'	,
-  //'{$mysqli->real_escape_string(isset($_POST['difficulty'])    ? $_POST['difficulty']   : '')}'	,
-  //'{$mysqli->real_escape_string(isset($_POST['directions'])    ? $_POST['directions']   : '')}'
-  //)";
+				    //User ID is hardcoded for now to show functionality
+				    $userID = '1633040277499'
+				    $RecipeName = (isset($_POST['recipeTitle'])    ? $_POST['recipeTitle']   : '');
+				    $gfree = (isset($_POST['gfree'])    ? $_POST['gfree']   : '');
+				    $feeds = (isset($_POST['feeds'])    ? $_POST['feeds']   : '');
+				    $difficulty = (isset($_POST['difficulty'])    ? $_POST['difficulty']   : '');
+				    $directions = (isset($_POST['directions'])    ? $_POST['directions']   : '');
+
+				// Insert our data
+				$sql = "INSERT INTO recipes( userID,RecipeName,gfree,feeds,difficulty,directions)
+				  VALUES ('$userID','$RecipeName','$gfree','$feeds','$difficulty','$directions')";
+				  //VALUES (
+				  //'{$mysqli->real_escape_string(isset($_POST['userID'])    ? $_POST['userID']   : '')}' ,
+				  //'{$mysqli->real_escape_string(isset($_POST['RecipeName'])    ? $_POST['RecipeName']   : '')}'	,
+				  //'{$mysqli->real_escape_string(isset($_POST['gfree'])    ? $_POST['gfree']   : '')}' 	,
+				  //'{$mysqli->real_escape_string(isset($_POST['feeds'])    ? $_POST['feeds']   : '')}'	,
+				  //'{$mysqli->real_escape_string(isset($_POST['difficulty'])    ? $_POST['difficulty']   : '')}'	,
+				  //'{$mysqli->real_escape_string(isset($_POST['directions'])    ? $_POST['directions']   : '')}'
+				  //)";
 
 
-$insert = $conn->query($sql);
+				$insert = $conn->query($sql);
 
-// Print response from MySQL
-if ( $insert === TRUE ) {
-echo "<h2>Recipe Submission Success!</h2>";
-} else {
-echo "Error: ". $sql ."<br>" . $conn-error;
-}
+				// Print response from MySQL
+				if ( $insert === TRUE ) {
+				echo "<h2>Recipe Submission Success!</h2>";
+				} else {
+				echo "Error: ". $sql ."<br>" . $conn-error;
+				}
 
-mysqli_close($conn);
-  ?>
-</div>
-</body>
-</html>
+				mysqli_close($conn);
+				  ?>
+				<div data-role="footer">
+					<h4>Tyson Funk&copy; 2016</h4>
+				</div>
+			</div>
+		</div>
+	</body>
+	</html>
