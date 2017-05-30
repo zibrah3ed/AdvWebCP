914817b5afbf165f92c0b8f2ccdd6f4e90161a76
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-05-29 19:09:14 -0500

    Add query

--- woohoo.php
+++ woohoo.php
@@ -10,10 +10,16 @@
   } else {
     echo "<h1>Successful Recipe Addtion!</h1>";
   }
-  
+
   // Use Hard coded values to check functionality
   $sql = "INSERT INTO recipes( userID,RecipeName,gfree,feeds,difficulty,directions)
    VALUES (1234567890,'Help',1,1,1,"hello world")";
 
-  mysqli_close($conn);
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+   $conn->close();
 ?>
