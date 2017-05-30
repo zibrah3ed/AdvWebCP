53c75cc2fee2b28a133eefc02b92a4def7bcd5b4
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-05-29 19:08:02 -0500

    Switch from proc to OO

--- woohoo.php
+++ woohoo.php
@@ -1,17 +1,19 @@
 <?php
-include 'config.php';
-$conn = new mysqli($dbhost, $dbuser, $dbpass);
+  // Connection String working
+  include 'config.php';
+  $conn = new mysqli($dbhost, $dbuser, $dbpass);
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-} else {
-  echo "<h1>PARTY</h1>";
-}
-// Use Hard coded values to check functionality
-//mysqli_query($conn,"INSERT INTO recipes( userID,RecipeName,gfree,feeds,difficulty,directions)
-//  VALUES (1234567890,'Help',1,1,1,"hello world")");
+  /* check connection */
+  if (mysqli_connect_errno()) {
+      printf("Connect failed: %s\n", mysqli_connect_error());
+      exit();
+  } else {
+    echo "<h1>Successful Recipe Addtion!</h1>";
+  }
+  
+  // Use Hard coded values to check functionality
+  $sql = "INSERT INTO recipes( userID,RecipeName,gfree,feeds,difficulty,directions)
+   VALUES (1234567890,'Help',1,1,1,"hello world")";
 
-mysqli_close($conn);
+  mysqli_close($conn);
 ?>
