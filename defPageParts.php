7fec54054a07d41c89dd6800fc53cf8e0038ff6c
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-08-21 15:10:48 -0500

    add signet button to footer mainpage

--- defPageParts.php
+++
@@ -1,7 +0,0 @@
-<?php
-function makeFooter(){
-echo "<div data-role='footer' data-position='fixed' data-fullscreen='true' class='footer'>
-    <p>Tyson Funk&copy; 2017</p><button onclick='signOut()' type='button' class='btn'>Sign Out</button>
-</div>";
-};
-?>
