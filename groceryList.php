760493f1bf3a725321985c921ee6cfdd57684ac3
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-08-26 16:55:28 -0500

    logo css

--- groceryList.php
+++ groceryList.php
@@ -30,7 +30,6 @@
       	</center>
    		</div>
 			<div data-role="content" data-theme="a" style="max-width: 100%;">
-				<div data-role="content" data-theme="d" style="max-width: 90%;">
                 <?php
                 // Create connection
 
@@ -82,7 +81,7 @@
 							</table>
 						</center>
 					</div>
-					</div>
+
       <!-- AddToAny BEGIN Code adapted from code provided by https://www.addtoany.com/buttons/for/website -->
                   <div class="a2a_kit a2a_kit_size_32 a2a_default_style text-center">
                   <!-- 	1. Required float and margin edits to render properly
