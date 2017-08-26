c973bb4d22113f066383e62e70d83e6aca09c9ed
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-08-26 14:07:57 -0500

    add recipe search page and default footer

---
+++ defaultPageParts.php
@@ -0,0 +1,6 @@
+<?php
+function createFooter() {
+  echo "<div data-role='footer' data-position='fixed' data-fullscreen='true' class='footer'>
+      <p>Tyson Funk&copy; 2017</p><button onclick='signOut()' type='button' class='btn'>Sign Out</button>
+  </div>";
+};
--- searchRecipes.php
+++ searchRecipes.php
@@ -40,6 +40,7 @@
 
                 include 'config.php';
                 include 'opendb.php';
+								include 'defaultPageParts.php';
 								$userID = 1;
 
 
@@ -93,9 +94,7 @@
   			<!-- AddToAny END -->
 
 
-				<div data-role="footer" data-position="fixed" data-fullscreen="true" class="footer">
-				    <p>Tyson Funk&copy; 2017</p><button onclick='signOut()' type="button" class="btn">Sign Out</button>
-				</div>
+    <?php createFooter; ?>
   </div>
     </div>
   </div>
