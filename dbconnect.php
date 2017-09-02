9eb77d8a5edb01232e8c1e8d5f00a1fabf1c39b7
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-09-02 11:07:11 -0500

    add login logout pages

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+// Create connection
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+$dbcon = mysqli_select_db(users);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if(!$dbcon){
+  die("Database connection failed " . mysqli_connect_error());
+}
+?>
