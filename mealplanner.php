63102e1cbf07e246f4983168fab321949f0c0d34
Tyson Funk <tyson.funk@smail.rasmussen.edu>
2017-09-02 13:34:16 -0500

    include defaultpageparts

--- mealplanner.php
+++ mealplanner.php
@@ -3,6 +3,7 @@
  session_start();
  include 'config.php';
  include 'opendb.php';
+ include 'defaultPageParts.php';
  // if session is not set this will redirect to login page
  if( !isset($_SESSION['user']) ) {
   header("Location: index.php");
